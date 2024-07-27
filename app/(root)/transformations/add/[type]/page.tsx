c03ff9b33e2946fc8b917218e5d5a5d12930dd8c
Pawanguptapawan <pawanguptapawan740@gmail.com>
2024-07-27 12:31:21 +0530

    till image.actions.ts in lib folder

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -16,14 +16,15 @@ import Header from '@/components/shared/Header'
 import TransformationForm from '@/components/shared/TransformationForm';
 import { transformationTypes } from '@/constants'
 import { getUserById } from '@/lib/actions/user.actions';
-import { useAuth } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
+
 import { redirect } from 'next/navigation';
 
 const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
-  const { userId } = useAuth();
+  
   const transformation = transformationTypes[type];
-
-  if(!userId) redirect('/sign-in')
+const { userId }=auth()
+   if(!userId) redirect('/sign-in')
 
   const user = await getUserById(userId);
 
@@ -33,15 +34,17 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
-    
-      <section className="mt-10">
+<section
+className="mt-10"
+>
         <TransformationForm 
-          action={"Add"}
-          userId={user._id}
-          type={transformation.type as TransformationTypeKey}
-          creditBalance={user.creditBalance}
+        action='Add'
+        userId={user._id}
+type={transformation.type as TransformationTypeKey}
+creditBalance={user.creditBalance}
         />
-      </section>
+        </section>
+     
     </>
   )
 }
